7cd47537f8911c9fb8cffe68031b31ca1a54a012
rinsarmu <roberaensermu@gmail.com>
2023-01-04 15:04:32 +0300

    Registering routes for optional

--- 08-Optional enhancing the app/routes/admin.js
+++ 08-Optional enhancing the app/routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express')
 const path = require("path")
 const router = express.Router();
 const rootDir = require('../utils/helper');
-const productController = require('../controllers/product')
+const adminController = require('../controllers/adminController')
 const products = [];
 
-router.get("/add-product",productController.getAddProduct )
+router.get("/add-product",adminController.getAddProduct )
 
-router.get('/products',productController.getAdminProducts)
-router.post('/product',productController.postAddProduct)
+router.get('/products',adminController.getProduct)
+router.post('/product',adminController.postAddProduct)
 
 module.exports = router;
--- 08-Optional enhancing the app/routes/shop.js
+++ 08-Optional enhancing the app/routes/shop.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../utils/helper');
 const adminData = require('./admin')
-const productController = require('../controllers/product')
-router.get('/', productController.getProduct ) 
+const shopController = require('../controllers/shopController')
+router.get('/', shopController.getIndex ) 
+router.get('/products', shopController.getProduct ) 
+router.get('/cart', shopController.getCart ) 
+router.get('/checkout', shopController.getCheckOut ) 
+
 module.exports = router;
