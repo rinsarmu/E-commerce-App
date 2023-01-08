65542b3bf9e18dc5a392ed04540c2ca1964dfaed
rinsarmu <roberaensermu@gmail.com>
2023-01-08 19:06:56 +0300

    DB

--- 10-SQL introduction/app.js
+++ 10-SQL introduction/app.js
@@ -4,6 +4,7 @@ const adminRoutes = require("./routes/admin")
 const shopRoutes = require('./routes/shop')
 const notFound = require('./routes/404')
 const path = require("path")
+const db = require('./utils/database')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -11,6 +12,7 @@ app.set('views', 'views')
 app.use(bodyparser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public'))) // should be execute on the top unless it's not working
 
+db.execute('Select * from products where ')
 //Routing
 app.use('/admin', adminRoutes)  // add product page and redirect to product page
 app.use(shopRoutes) // Product page
--- 10-SQL introduction/utils/database.js
+++ 10-SQL introduction/utils/database.js
@@ -1,1 +1,9 @@
 const mysql = require('mysql2')
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'sys',
+    password: 'root@123'
+})
+
+module.exports = pool.promise();
