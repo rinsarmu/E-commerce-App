cd53e7a0c4d685a1de3847724ecc55a9046c8bce
rinsarmu <roberaensermu@gmail.com>
2023-01-09 16:16:45 +0300

    restructuring file for db work fine

--- 10-SQL introduction/models/product.js
+++ 10-SQL introduction/models/product.js
@@ -1,22 +1,6 @@
-const fs = require('fs')
-const path = require('path');
-const rootDir = require('../utils/helper')
-const Cart = require('../models/cart')
 
-
-const p = path.join(rootDir, 'data', 'product.json') 
-
-const getProductFromFile = (cb) =>{
-    fs.readFile(p, (err, data)=>{
-        if(err) {
-            cb([])
-        } else  {
-            cb(JSON.parse(data))
-        }
-       
-      })
-}
-
+const db = require('../utils/database')
+const Cart = require('../models/cart');
 
 module.exports = class Product {
     constructor(id,title, description, price, imageUrl){
@@ -27,53 +11,17 @@ module.exports = class Product {
         this.imageUrl = imageUrl
     }
     save() {
-        getProductFromFile(products=>{
-            if(this.id) {
-                console.log("product ID: ", this.id);
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-
-                console.log("existingProductIndex : ", existingProductIndex);
-                const updatedProduct = [...products];
-                updatedProduct[existingProductIndex] = this
-            fs.writeFileSync(p,JSON.stringify(updatedProduct));
-
-            } else{
-
-            this.id = (Math.random()+ 1).toString();
-            products.push(this)
-            fs.writeFileSync(p,JSON.stringify(products));
-        }
-
-           
-        })
-                
+        
     }
 
     static fetchAll(cb) {
-        getProductFromFile(cb);
-      }
+    }
 
-      static delete(id){
-        // console.log("deletehhhhhhhhhhhhhh");
-        getProductFromFile(products=>{
-           const updatedProduct = products.filter(prod=>prod.id!= id)
-           const product = products.find(prod=>prod.id == id)
-        //    console.log(product.price);
-           fs.writeFile(p, JSON.stringify(updatedProduct), err=>{
-            if(!err){
-                Cart.updatingProduct(id, product.price)
-
-            }
-           })
-       })
+    static delete(id){
+     
    }
    
 
-    static findById(id, cb){
-        getProductFromFile(products =>{
-            const product = products.find(p => p.id == id)
-            cb(product)
-        })
+    static findById(id){
     }
-
 }
